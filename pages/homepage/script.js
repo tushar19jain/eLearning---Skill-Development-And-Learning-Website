72051c6c1ca9f208960f9f9fe1b59cf64ef105b9
tushar19jain <jaint381@gmail.com>
2023-07-17 21:54:23 +0530

    Added server.js,index.html,style.css,script.js

---
+++ pages/homepage/script.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var time = today.getHours();
+console.log(time);
+if(time >= 12 && time <=17){
+    document.getElementById('greeting').innerText = "Good Afternoon";
+}
+else if(time <=12){
+    document.getElementById('greeting').innerText = "Good Morning";
+}
+else{
+    document.getElementById('greeting').innerText = "Good Evening";
+}
