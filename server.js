42e0f124c44186cfe5231493fb59a59f74294595
tushar19jain <jaint381@gmail.com>
2023-07-19 19:34:22 +0530

    Added mongo db and a popup Login/Signup page

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const {MongoClient} = require('mongodb');
+const url = 'mongodb+srv://jaint381:IxcoZULZH78cacCt@e-learning.hvqtogq.mongodb.net/'
+const database = 'users';
+const client = new MongoClient(url);
+
+client.connect();
+console.log('Connection successfull');
+const db = client.db(database);
+const collection = db.createCollection('Users')
+client.close()
+
 app.use(express.static('pages'));
 app.get('/',(req,res)=>{
     res.sendFile('pages/homepage/index.html',{root:__dirname});
